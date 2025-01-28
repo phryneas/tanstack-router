a09f3bcba8e8a167eaad235d726ccea1a4fc09f1
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-01-28 10:13:40 +0100

    reproduction: consuming the stream in SSR results in "Invalid state: The ReadableStream is locked"

--- e2e/start/basic/app/routes/stream.tsx
+++ e2e/start/basic/app/routes/stream.tsx
@@ -1,5 +1,5 @@
 import { Await, createFileRoute } from '@tanstack/react-router'
-import { useEffect, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 
 export const Route = createFileRoute('/stream')({
   component: Home,
@@ -21,41 +21,79 @@ export const Route = createFileRoute('/stream')({
   },
 })
 
-const decoder = new TextDecoder('utf-8')
+interface StreamData {
+  accumulatedData: Array<string>
+  subscribe: (cb: (accumulatedData: Array<string>) => void) => () => void
+  initialValuePromise: Promise<string>
+  finishedPromise: Promise<Array<string>>
+}
+const streamDataMap = new Map<ReadableStream, StreamData>()
+
+function getStreamData(stream: ReadableStream): StreamData {
+  let streamData = streamDataMap.get(stream)
+  if (streamData) return streamData
+
+  const subscriptions = new Set<(accumulatedData: Array<string>) => void>()
+  let resolveFirstChunk!: (value: string) => void
+  let resolveFinished!: (value: Array<string>) => void
+  streamData = {
+    accumulatedData: [],
+    subscribe(callback) {
+      subscriptions.add(callback)
+      return () => subscriptions.delete(callback)
+    },
+    initialValuePromise: new Promise((resolve) => {
+      resolveFirstChunk = resolve
+    }),
+    finishedPromise: new Promise((resolve) => {
+      resolveFinished = resolve
+    }),
+  }
+
+  streamDataMap.set(stream, streamData)
+  ;(async function receiveData() {
+    const reader = stream.pipeThrough(new TextDecoderStream()).getReader()
+
+    let chunk
+    while (!(chunk = await reader.read()).done) {
+      const value = chunk.value
+      streamData.accumulatedData.push(value)
+      resolveFirstChunk(value)
+      for (const subscription of subscriptions) {
+        subscription(streamData.accumulatedData)
+      }
+    }
+    resolveFinished(streamData.accumulatedData)
+  })()
+
+  return streamData
+}
 
 function Home() {
   const { promise, stream } = Route.useLoaderData()
-  const [streamData, setStreamData] = useState([])
+  const [streamedValues, setStreamedValues] = useState<Array<string>>([])
+
+  const streamData = getStreamData(stream)
+  use(streamData.initialValuePromise)
 
   useEffect(() => {
-    async function fetchStream() {
-      const reader = stream.getReader()
-      let chunk
-
-      while (!(chunk = await reader.read()).done) {
-        const decoded = decoder.decode(chunk.value, { stream: !chunk.done })
-        setStreamData((prev) => [...prev, decoded])
-      }
-    }
-
-    fetchStream()
+    return streamData.subscribe(setStreamedValues)
   }, [])
 
   return (
     <>
+      <div className="p-2" data-testid="promise-data">
+        <div data-testid="stream-data">
+          {streamedValues.map((d) => (
+            <div key={d}>{d}</div>
+          ))}
+        </div>
+      </div>
       <Await
-        promise={promise}
-        children={(promiseData) => (
-          <div className="p-2" data-testid="promise-data">
-            {promiseData}
-            <div data-testid="stream-data">
-              {streamData.map((d) => (
-                <div key={d}>{d}</div>
-              ))}
-            </div>
-          </div>
-        )}
+        promise={streamData.finishedPromise}
+        children={() => 'stream finished'}
       />
+      <Await promise={promise} children={(promiseData) => promiseData} />
     </>
   )
 }
